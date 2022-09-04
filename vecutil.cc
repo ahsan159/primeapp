af16c1f473ab10a475a1234052eaa51c5b0a5bc4
masaddique <eemasaddique@gmail.com>
2022-09-04 07:28:20 +0500

    first code commit

---
+++ vecutil.cc
@@ -0,0 +1,22 @@
+#include "vecutil.hh"
+
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <cstdlib>
+
+int addition() {
+  return 1;
+}
+
+int subtraction() {
+  return -1;
+}
+
+int multiplication() {
+  return 2;
+}
+
+int division() {
+  return 0;
+}
---
+++ vecutil.hh
@@ -0,0 +1,9 @@
+#ifndef _VECTUTIL_H_
+#define _VECTUTIL_H_
+
+int addition();
+int subtraction();
+int multiplication();
+int division();
+
+#endif 
